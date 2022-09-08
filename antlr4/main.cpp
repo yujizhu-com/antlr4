b7c27ae42feb1ebb8465663473454a3fdc899433
余纪柱 <1440198508@qq.com>
2022-09-08 15:26:06 +0800

    提交测试代码

--- antlr4/main.cpp
+++ antlr4/main.cpp
@@ -8,8 +8,32 @@
 #include <antlr4-runtime.h>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-
-    
+#include "ExprBaseListener.h"
+#include "ExprBaseVisitor.h"
+#include "ExprLexer.h"
+int main(int argc, const char * argv[])
+{
+    ExprParser* parser = new ExprParser(NULL);
+    parser->setBuildParseTree(false);
+    std::string expr = "100+2*34";
+    antlr4::ANTLRInputStream* input = new antlr4::ANTLRInputStream(expr+"\n");
+    ExprLexer* lexer = new ExprLexer(input);
+    lexer->setLine(1);
+    lexer->setCharPositionInLine(0);
+    antlr4::CommonTokenStream* tokens = new antlr4::CommonTokenStream(lexer);
+    parser->setInputStream(tokens);
+//    while ( expr!=null )
+//    {
+//        ANTLRInputStream input = new ANTLRInputStream(expr+"\n");
+//        ExprLexer lexer = new ExprLexer(input);
+//        lexer.setLine(line);
+//        lexer.setCharPositionInLine(0);
+//        CommonTokenStream tokens = new CommonTokenStream(lexer);
+//        parser.setInputStream(tokens);
+//        parser.stat();
+//        expr = br.readLine();
+//        line++;
+//    }
+//    auto ep = ExprParser(antlr4::TokenStream *input);
     return 0;
 }
