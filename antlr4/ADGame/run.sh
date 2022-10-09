ca283861c1c5d7e0592551abe6e298c8d6cd1b35
余纪柱 <1440198508@qq.com>
2022-10-09 11:13:30 +0800

    脚本优化

--- antlr4/ADGame/run.sh
+++ antlr4/ADGame/run.sh
@@ -1,7 +1,10 @@
 #!bin/bash
-
+from="/Users/yujizhu/Documents/Git/Github/antlr4/antlr4/ADGame/";
+to="/Users/yujizhu/Documents/Git/Github/antlr4/antlr4/gammar/";
+cp $from"Expr.g4"  $to"Expr.g4"
+cp $from"Expr.txt" $to"Expr.txt" 
+antlr -Dlanguage=Cpp -visitor -listener  -o /Users/yujizhu/Documents/Git/Github/antlr4/antlr4/gammar Expr.g4
 antlr Expr.g4    
 javac *.java        
 grun Expr $1 -gui Expr.txt
-antlr -Dlanguage=Cpp -visitor -listener  -o /Users/yujizhu/Documents/Git/Github/antlr4/antlr4/gammar Expr.g4
 
