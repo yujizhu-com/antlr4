443ee72e72b92be790b8d7f5921e5e66a032894c
余纪柱 <1440198508@qq.com>
2022-10-09 14:20:36 +0800

    清除无用代码

--- antlr4/main.cpp
+++ antlr4/main.cpp
@@ -45,43 +45,5 @@ int main(int argc, const char * argv[])
     
     ExprBaseVisitor* dv = new DicsVisitor();
     dv->visit(tree);
-
-//    {
-//        std::string progStr =  "(100+5)\n";
-//        antlr4::ANTLRInputStream* input = new antlr4::ANTLRInputStream(progStr);
-//        ExprLexer* lexer = new ExprLexer(input);
-//        antlr4::CommonTokenStream* tokens = new antlr4::CommonTokenStream(lexer);
-//
-//        ExprParser* parser = new ExprParser(tokens);
-//        std::cout << parser->prog()->toStringTree(parser) << std::endl;
-//
-//    }
-//    {
-//        std::string exprStr = "(100)";
-//        antlr4::ANTLRInputStream* input = new antlr4::ANTLRInputStream(exprStr);
-//        ExprLexer* lexer = new ExprLexer(input);
-//        antlr4::CommonTokenStream* tokens = new antlr4::CommonTokenStream(lexer);
-//        ExprParser* parser = new ExprParser(tokens);
-//        std::cout <<  parser->expr()->toStringTree(parser) << std::endl;
-//    }
-//    {
-//        std::string exprStr = "(100+5)";
-//        antlr4::ANTLRInputStream* input = new antlr4::ANTLRInputStream(exprStr);
-//        ExprLexer* lexer = new ExprLexer(input);
-//        antlr4::CommonTokenStream* tokens = new antlr4::CommonTokenStream(lexer);
-//        ExprParser* parser = new ExprParser(tokens);
-//        std::cout <<  parser->expr()->toStringTree(parser) << std::endl;
-//
-//        auto walker = new antlr4::tree::ParseTreeWalker();
-//        walker->walk(new ExprBaseListener(), parser->expr());
-//
-////        auto listener = new ExprBaseListener();
-//
-////        auto visitor = new ExprBaseVisitor();
-////        visitor->visitExpr(parser->prog());
-////
-//    }
-    
-    
     return 0;
 }
